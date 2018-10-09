f950ac7f258b3fd2328c46dc066a71a665d02651
Agris Sostaks <Agris.Sostaks@lumii.lv>
2018-10-09 23:23:07 +0300

    Add condition ... menu added

---
+++ client/js/custom/add_condition_form.js
@@ -0,0 +1,33 @@
+Interpreter.customMethods({
+	AddCondition: function () {
+
+		$("#add-condition-form").modal("show");
+		$('#condition-expression').val('');
+		//$('#aggregate-expression').val('');
+	//	$('input[name=stack-checkbox-req]').attr('checked',false);
+	}
+})
+
+
+Template.AddCondition.helpers({
+
+});
+
+
+Template.AddCondition.events({
+
+	"click #ok-add-condition": function(e) {
+
+		var selected_elem_id = Session.get("activeElement");
+		if (Elements.findOne({_id: selected_elem_id})){ //Because in case of deleted element ID is still "activeElement"
+		//Read user's choise
+		  var vq_obj = new VQ_Element(selected_elem_id);
+			var condition = $('#condition-expression').val();
+			vq_obj.addCondition(condition);
+	  };
+
+		return;
+
+	},
+
+});
