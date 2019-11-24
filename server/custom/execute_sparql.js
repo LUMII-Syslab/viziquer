1c1abbcab871982b44f174de00702574a33acbab
mikus <mikusg@gmail.com>
2019-11-24 02:00:56 +0200

    sparql execution tuning

--- server/custom/execute_sparql.js
+++ server/custom/execute_sparql.js
@@ -43,13 +43,12 @@ Meteor.methods({
 
                 let namedGraph = count_options.params.params['default-graph-uri'];
 
-                // let httpOptions = _.extend({}, authOptions);
-                let httpOptions = _.extend({}, count_options.params, authOptions); // ?? vai count_options.params var saturēt ko noderīgu?
+                let httpOptions = _.extend({}, authOptions);
+                // let httpOptions = _.extend({}, count_options.params, authOptions); // ?? vai count_options.params var saturēt ko noderīgu?
 
                 var qres = HTTP_REQUEST_FN(count_options.endPoint, httpOptions, query, namedGraph, true);
 
-                if (qres.statusCode == 200) {
-                    console.log(qres.content); //££//
+                if (qres.statusCode === 200) {
                     var content = JSON.parse(qres.content);
                     number_of_rows = content.results.bindings[0].number_of_rows_in_query_xyz.value;
                     _.extend(sparql_log_entry, {successfull:true});
@@ -90,7 +89,7 @@ Meteor.methods({
             let query = options.params.params.query;
             let namedGraph = options.params.params['default-graph-uri'];
 
-            query = encodeQuery(query);
+            // query = encodeQuery(query);
             // options.endPoint = options.endPoint + '?'+ 'default-graph-uri=' + namedGraph +'&query=' + query;
 
             let httpOptions = _.extend({}, authOptions);
@@ -103,13 +102,11 @@ Meteor.methods({
                 }
                 else {
                     if( resp["statusCode"] == 200 && resp["headers"]["content-type"].toLowerCase().startsWith("text")){
-                        console.log(resp["statusCode"], resp["headers"]["content-type"], resp.content);
                         var error_message = resp.content;
                         if(error_message.length > 514) error_message = error_message.substring(0, 514) + "...";
                         future.return({status: 504, error:resp.content, limit_set: false, number_of_rows: 0});
                     } else{
                         xml2js.parseString(resp.content, function(json_err, json_res) {
-                            console.log(resp["statusCode"], resp["headers"]["content-type"], resp.content, json_res);
                             if (json_err) {
                                 future.return({status: 504, error:json_err, limit_set: false, number_of_rows: 0});
                             }
@@ -240,14 +237,22 @@ Meteor.methods({
 });
 
 function detectContentType(content) {
-    let text = content.toLowerCase();
-    if (text.startsWith('<?xml')) return 'xml';
-    if (text.startsWith('<sparql')) return 'xml';
-    if (text.startsWith('{')) return 'json'; // REFINE ME
-    if (text.startsWith('<!doctype')) return 'html';
-    if (text.startsWith('<html')) return 'html';
-    if (text.startsWith('<!--')) return 'html';
-    return 'text';
+    if (!content) {
+        return 'empty';
+    }
+    if (typeof content !== 'string') {
+        return 'not a string';
+    }
+
+    let text = content.toLowerCase().trim();
+    let ct = 'text';
+    if (text.startsWith('<?xml')) ct = 'xml';
+    if (text.startsWith('<sparql')) ct = 'xml';
+    if (text.startsWith('{')) ct = 'json'; // REFINE ME
+    if (text.startsWith('<!doctype')) ct = 'html';
+    if (text.startsWith('<html')) ct = 'html';
+    if (text.startsWith('<!--')) ct = 'html';
+    return `${ct} (${text.length} chars, "${text.length < 32 ? text : text.slice(0, 32) + '...'}")`;
 }
 
 function selectHttpRequestProfile(options) {
@@ -259,50 +264,91 @@ function selectHttpRequestProfile(options) {
     }
 }
 
-var DEFAULT_PROFILE_NAME = "P2";
+var PROFILE_MAP = {
+    P1: doHttpRequestP1,
+    P1b: doHttpRequestP1b,
+    P2: doHttpRequestP2,
+    P2b: doHttpRequestP2b,
+    P3: doHttpRequestP3,
+    P4: doHttpRequestP4,
+};
+
+var DEFAULT_PROFILE_NAME = "P2"; // <-- change here to switch the default profile for http requests
+var DEFAULT_PROFILE = PROFILE_MAP[DEFAULT_PROFILE_NAME];
 
 var NON_DEFAULT_PROFILES = [
     { pattern: 'wikidata.org', profileName: 'P1' },
-    { pattern: 'scholarlydata.org', profilName: 'P4' },
+    { pattern: 'scholarlydata.org', profileName: 'P4' },
 ];
 
 var TIMEOUT = 0;
 
 var XML_FORMAT = 'application/sparql-results+xml';
 var JSON_FORMAT = 'application/sparql-results+json';
+var XML_FORMAT_SHORT = 'xml';
+var JSON_FORMAT_SHORT = 'json';
 
-var ENDPOINT_TEST_QUERY = 'SELECT ?a ?b ?c where{?a ?b ?c} LIMIT 10';
+var USER_AGENT = 'ViziQuer 0.x';
+
+// var ENDPOINT_TEST_QUERY = 'SELECT ?a ?b ?c where{?a ?b ?c} LIMIT 10';
+var ENDPOINT_TEST_QUERY = 'SELECT (COUNT(*) AS ?number_of_rows_in_query_xyz) WHERE { SELECT ?a ?b ?c where{?a ?b ?c} LIMIT 10 }';
+
+var COMMON_HEADERS = {
+    'User-Agent': USER_AGENT,
+    'Cache-Control': 'no-cache',
+};
 
 function selectHttpRequestProfileByName(name) {
     if (!name) return DEFAULT_PROFILE;
-
-    switch(name) {
-        case 'P1':
-            return doHttpRequestP1;
-        case 'P1b':
-            return doHttpRequestP1b;
-        case 'P2':
-            return doHttpRequestP2;
-        case 'P2b':
-            return doHttpRequestP2b;
-        case 'P3':
-            return doHttpRequestP3;
-        case 'P4':
-            return doHttpRequestP4;
-        default:
-            return selectHttpRequestProfileByName(DEFAULT_PROFILE_NAME);
+    if (PROFILE_MAP[name]) {
+        console.log(`Profile ${name} selected`);
+        return PROFILE_MAP[name];
     }
+    console.log(`Default profile ${DEFAULT_PROFILE_NAME} selected`);
+    return DEFAULT_PROFILE;
 }
 
 function selectHttpRequestProfileNameByUrl(url) {
     for (let entry of NON_DEFAULT_PROFILES) {
-        if (url.indexOf(entry.pattern) !== -1) return entry.profilName;
+        if (url.indexOf(entry.pattern) !== -1) return entry.profileName;
     }
 
     return DEFAULT_PROFILE_NAME;
 }
 
+var DO_CALL_DEBUG = function(method, url, options, cb) {
+    console.log('☕', method, url, options);
+    if (cb) {
+        HTTP.call(method, url, options, (err, resp) => {
+            if (resp.statucCode >= 300) {
+                console.log('🥤🥤', err, resp);
+            }
+            console.log('🦊🦊', resp["statusCode"], resp["headers"]["content-type"], detectContentType(resp.content));
+            cb(err, resp);
+        });
+    } else {
+        const resp = HTTP.call(method, url, options);
+        if (resp.statucCode >= 300) {
+            console.log('🥤', err, resp);
+        }
+        console.log('🦊', resp["statusCode"], resp["headers"]["content-type"], detectContentType(resp.content));
+        return resp;
+    }
+}
+
+var DO_CALL_PROD = function(method, url, options, cb) {
+    if (cb) {
+        HTTP.call(method, url, options, cb);
+    } else {
+        return HTTP.call(method, url, options);
+    }
+}
+
+// var DO_CALL = DO_CALL_DEBUG;
+var DO_CALL = DO_CALL_PROD;
+
 // NOTE: Blazegraph does not like an empty value for the parameter 'default-graph-uri'.
+// NOTE: Blazegraph seems to like User-Agent.
 
 /**
  * HTTP request profiles for calling SPARQL endpoints:
@@ -315,25 +361,21 @@ function selectHttpRequestProfileNameByUrl(url) {
 
 function doHttpRequestP1(url, httpOptions, query, namedGraph, preferJSON, callback) {
     console.log("profile P1", url, query, namedGraph, httpOptions, preferJSON);
-    let fullUrl = `${url}?query=${encodeQuery2(query)}`;
+    // let fullUrl = `${url}?query=${encodeQuery2(query)}`;
+    let fullUrl = `${url}?query=${encodeQueryForUrl(query)}`;
     if (namedGraph) {
         fullUrl += `&default-graph-uri=${encodeURIComponent(namedGraph)}`;
     }
     let fullOptions = _.extend({}, httpOptions, { timeout: TIMEOUT });
-    fullOptions.headers = {};
+    fullOptions.headers = _.extend({}, COMMON_HEADERS);
     if (preferJSON) {
-        // fullUrl += `&format=${JSON_FORMAT}`;
+        fullUrl += `&format=${JSON_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullUrl += `&format=${XML_FORMAT}`;
+        fullUrl += `&format=${XML_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("get P1", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.get(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.get(fullUrl, fullOptions);
-    }
+    return DO_CALL('GET', fullUrl, fullOptions, callback);
 }
 
 function doHttpRequestP1b(url, httpOptions, query, namedGraph, preferJSON, callback) {
@@ -348,56 +390,51 @@ function doHttpRequestP1b(url, httpOptions, query, namedGraph, preferJSON, callb
         fullOptions.params['default-graph-uri'] = namedGraph;
     }
 
-    fullOptions.headers = {};
+    fullOptions.headers = _.extend({}, COMMON_HEADERS);
     if (preferJSON) {
-        // fullUrl += `&format=${JSON_FORMAT}`;
+        fullOptions.params['format'] = JSON_FORMAT_SHORT;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullUrl += `&format=${XML_FORMAT}`;
+        fullOptions.params['format'] = XML_FORMAT_SHORT;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("get P1b", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.get(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.get(fullUrl, fullOptions);
-    }
+    return DO_CALL('GET', fullUrl, fullOptions, callback);
 }
 
 function doHttpRequestP2(url, httpOptions, query, namedGraph, preferJSON, callback) {
     console.log("profile P2", url, query, namedGraph, httpOptions, preferJSON);
     let fullUrl = url;
     let fullOptions = _.extend({}, httpOptions, { timeout: TIMEOUT });
-    fullOptions.headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
-    // fullOptions.content = `query=${encodeQuery(query)}`;
-    fullOptions.content = `query=${encodeQuery2(query)}`;
+    fullOptions.headers = _.extend(
+        {},
+        COMMON_HEADERS,
+        { 'Content-Type': 'application/x-www-form-urlencoded' }
+    );
+    fullOptions.content = `query=${encodeQueryForUrl(query)}`;
+    // fullOptions.content = `query=${encodeQueryForBody(query)}`;
     // fullOptions.content = `query=${query}`;
     if (namedGraph) {
         fullOptions.content += `&default-graph-uri=${encodeURIComponent(namedGraph)}`;
     }
     if (preferJSON) {
-        // fullOptions.content += `&format=${JSON_FORMAT}`;
+        fullOptions.content += `&format=${JSON_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullOptions.content += `&format=${XML_FORMAT}`;
+        fullOptions.content += `&format=${XML_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("post P2", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.post(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.post(fullUrl, fullOptions);
-    }
+    return DO_CALL('POST', fullUrl, fullOptions, callback);
 }
 
 function doHttpRequestP2b(url, httpOptions, query, namedGraph, preferJSON, callback) {
     console.log("profile P2b", url, query, namedGraph, httpOptions, preferJSON);
     let fullUrl = url;
     let fullOptions = _.extend({}, httpOptions, { timeout: TIMEOUT });
-    fullOptions.headers = {}
-    fullOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    fullOptions.headers = _.extend(
+        {},
+        COMMON_HEADERS,
+        { 'Content-Type': 'application/x-www-form-urlencoded' }
+    );
     fullOptions.params = {
         query: encodeQuery2(query),
         // query: query,
@@ -406,18 +443,13 @@ function doHttpRequestP2b(url, httpOptions, query, namedGraph, preferJSON, callb
         fullOptions.params['default-graph-uri'] = namedGraph
     }
     if (preferJSON) {
-        // fullOptions.content.format = JSON_FORMAT;
+        fullOptions.params.format = JSON_FORMAT_SHORT;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullOptions.content.format = XML_FORMAT;
+        fullOptions.params.format = XML_FORMAT_SHORT;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("post P2b", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.post(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.post(fullUrl, fullOptions);
-    }
+    return DO_CALL('POST', fullUrl, fullOptions, callback);
 }
 
 function doHttpRequestP3(url, httpOptions, query, namedGraph, preferJSON, callback) {
@@ -427,70 +459,108 @@ function doHttpRequestP3(url, httpOptions, query, namedGraph, preferJSON, callba
         fullUrl += `?default-graph-uri=${encodeURIComponent(namedGraph)}`;
     }
     let fullOptions = _.extend({}, httpOptions, { timeout: TIMEOUT });
-    fullOptions.headers = {
-        'Content-Type': 'application/sparql-query'
-    };
+    fullOptions.headers = _.extend(
+        {},
+        COMMON_HEADERS,
+        { 'Content-Type': 'application/sparql-query' }
+    );
     fullOptions.content = query;
     if (preferJSON) {
-        // fullUrl += `&format=${JSON_FORMAT}`;
+        fullUrl += `&format=${JSON_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullUrl += `&format=${XML_FORMAT}`;
+        fullUrl += `&format=${XML_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("post P3", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.post(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.post(fullUrl, fullOptions);
-    }
+    return DO_CALL('POST', fullUrl, fullOptions, callback);
 }
 
 function doHttpRequestP4(url, httpOptions, query, namedGraph, preferJSON, callback) {
     console.log("profile P4", url, query, namedGraph, httpOptions, preferJSON);
-    let fullUrl = `${url}?query=${encodeQuery2(query)}`;
+    let fullUrl = `${url}?query=${encodeQueryForUrl(query)}`;
     if (namedGraph) {
         fullUrl += `&default-graph-uri=${encodeURIComponent(namedGraph)}`;
     }
     let fullOptions = _.extend({}, httpOptions, { timeout: TIMEOUT });
-    fullOptions.headers = {};
+    fullOptions.headers = _.extend({}, COMMON_HEADERS);
     if (preferJSON) {
-        // fullUrl += `&format=${JSON_FORMAT}`;
+        fullUrl += `&format=${JSON_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = JSON_FORMAT;
     } else {
-        // fullUrl += `&format=${XML_FORMAT}`;
+        fullUrl += `&format=${XML_FORMAT_SHORT}`;
         fullOptions.headers['Accept'] = XML_FORMAT;
     }
-    console.log("post P4", fullUrl, fullUrl);
-    if (callback) {
-        HTTP.post(fullUrl, fullOptions, callback);
-    } else {
-        return HTTP.post(fullUrl, fullOptions);
-    }
+    return DO_CALL('POST', fullUrl, fullOptions, callback);
 }
 
-function encodeQuery(q) {
-    var query = q.replace(/(\r\n|\n|\r)/gm," ");
-    query = encodeURIComponent(query);
-    query = query.replace(/\*/g, '%2A');
-    query = query.replace(/\(/g, '%28');
-    query = query.replace(/\)/g, '%29');
-    return query;
+function encodeQueryForUrl(q) {
+    var query = removeMultilines(q);
+    return encodeURIComponent(query);
 }
 
-function encodeQuery2(q) {
-    var query = q.replace(/(\r\n|\n|\r)/gm," ");
-    query = query.replace(/\s/g, '+');
-    query = query.replace(/\?/g, '%3F');
-    query = query.replace(/\{/g, '%7B');
-    query = query.replace(/\}/g, '%7D');
-
-    // query = query.replace(/\*/g, '%2A');
-    // query = query.replace(/\(/g, '%28');
-    // query = query.replace(/\)/g, '%29');
-
-    return query;
+function encodeQueryForBody(q) {
+    var query = removeMultilines(q);
+    query = query.replace(/ /g, "+");
+    return encodeURIComponent(query);
 }
+
+function removeMultilines(q) {
+    return q
+        .replace(/(\r\n|\n|\r)/gm," ")
+        .replace(/  /g, " ")
+        .trim();
+}
+
+// function encodeQuery(q) {
+//     var query = q.replace(/(\r\n|\n|\r)/gm," ");
+//     query = encodeURIComponent(query);
+//     query = query.replace(/\*/g, '%2A');
+//     query = query.replace(/\(/g, '%28');
+//     query = query.replace(/\)/g, '%29');
+//     return query;
+// }
+
+// function encodeQuery2(q) {
+//     var query = q.replace(/(\r\n|\n|\r)/gm," ");
+//     query = query.replace(/\s/g, '+');
+//     query = query.replace(/\?/g, '%3F');
+//     query = query.replace(/\{/g, '%7B');
+//     query = query.replace(/\}/g, '%7D');
+
+//     // query = query.replace(/\*/g, '%2A');
+//     // query = query.replace(/\(/g, '%28');
+//     // query = query.replace(/\)/g, '%29');
+
+//     return query;
+// }
+
+// function encodeQuery3(q) {
+//     var query = q.replace(/(\r\n|\n|\r)/gm," ");
+
+//     // query = query.replace(/\s/g, '+');
+//     query = query.replace(/\s/g, '%20');
+//     query = query.replace(/\!/g, '%21');
+//     query = query.replace(/\#/g, '%23');
+//     query = query.replace(/\$/g, '%24');
+//     query = query.replace(/\%/g, '%25');
+//     query = query.replace(/\&/g, '%26');
+//     query = query.replace(/\'/g, '%27');
+//     query = query.replace(/\(/g, '%28');
+//     query = query.replace(/\)/g, '%29');
+//     query = query.replace(/\*/g, '%2A');
+//     query = query.replace(/\+/g, '%2B');
+//     query = query.replace(/\,/g, '%2C');
+//     query = query.replace(/\//g, '%2F');
+//     query = query.replace(/\:/g, '%3A');
+//     query = query.replace(/\;/g, '%3B');
+//     query = query.replace(/\=/g, '%3D');
+//     query = query.replace(/\?/g, '%3F');
+//     query = query.replace(/\@/g, '%40');
+//     query = query.replace(/\[/g, '%5B');
+//     query = query.replace(/\]/g, '%5D');
+
+//     return query;
+// }
 
 function isURL(s) {
 	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
