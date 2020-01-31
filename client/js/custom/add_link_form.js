eab9633b81e237b30f6d15b2d8aabb6c4165e425
JulijaH <mf90011@lanet.lv>
2020-01-31 14:35:46 +0200

    AddLink search upgrade
    AddLink now can search for multiple values from search box.
    Allowed separators: space and comma.
    Point is reserved for property path search.
    
    Additional, deactivated for this version - some changes to add button to switch to Connect Classes.

--- client/js/custom/add_link_form.js
+++ client/js/custom/add_link_form.js
@@ -19,16 +19,10 @@ Template.AddLink.helpers({
 	shortList: function(){
 		return Template.AddLink.shortList.get();
 	},
-
-	/*associations: function() {
-		asc = getAllAssociations();		
-		console.log(asc);
-		return asc;
-	},*/
-
 });
 
 Template.AddLink.events({
+//Buttons
 	"click #ok-add-link": function() {
 
 		//Read user's choise
@@ -103,261 +97,6 @@ Template.AddLink.events({
                 Template.AggregateWizard.endClassId.set(cl.obj._id);
             }, newPosition);
 
-/* O L D   V E R S I O N
-
-			//Initial coordinate values original box and new box
-			var d = 30;
-			var x0 = elem_start["location"]["x"];
-			var y0 = elem_start["location"]["y"];
-			var x1 = x0;
-			var y1 = y0 + elem_start["location"]["height"] + d;
-			var w = elem_start["location"]["width"];
-			var h = elem_start["location"]["height"];
-
-
-			//If diagram is populated - search for overlap
-			//Temporal solution: Put new element as low as possible, no packaging algorithm && elem_list["location"]
-
-			var elem_list = [];
-			var elem_over = []; //Potentionally - for more complex search for a better place
-			var max_y;
-
-			Elements.find({type: "Box"}).forEach(function(el) {
-				elem_list.push(el);
-			})
-
-			do{
-				elem_over.length = 0;
-
-				_.each(elem_list, function(el) {
-					//Check, if start point of existing element could lead to overlap withexisting elements
-					if (el["location"]["x"] < (x1+w)){
-						if (el["location"]["y"] < (y1+h)){
-							//Check, if end point of existing element could lead to overlap
-							if((el["location"]["x"]+el["location"]["width"]) > x1){
-								if((el["location"]["y"])+el["location"]["height"] > y1){
-									elem_over.push({
-										_id: el["_id"],
-										x: el["location"]["x"],
-										y: el["location"]["y"],
-										w: el["location"]["width"],
-										h: el["location"]["height"]
-									});
-								}
-							}
-						}
-					}
-				})
-
-				if (elem_over.length > 0){
-					max_y = 0;
-
-					_.each(elem_over, function(el){
-						if (max_y < (el["y"]+el["h"])) {
-							max_y = el["y"]+el["h"];
-						}
-					})
-
-					y1 = max_y + d;
-				}
-			} while (elem_over.length > 0);
-
-
-			//New elements
-			//Create Box; original constructor is used
-			var elem_type = ElementTypes.findOne({_id: Session.get("activeElementType")});
-			var elem_style = _.find(elem_type.styles, function(style) {
-									return style.name === "ConditionClass";
-								});
-
-			if (!elem_style) {
-				show_error_msg("Internal error: No element style");
-				return;
-			}
-
-			var new_box = {
-							projectId: Session.get("activeProject"),
-							versionId: Session.get("versionId"),
-
-							diagramId: Session.get("activeDiagram"),
-							diagramTypeId: elem_type["diagramTypeId"],
-							elementTypeId: elem_type["_id"],
-							style: {elementStyle: elem_style["elementStyle"]},
-							styleId: elem_style["id"],
-							type: "Box",
-							location:  {x: x1, y: y1, width: w, height: h}
-						};
-
-			Utilities.callMeteorMethod("insertElement", new_box, function(elem_id) {
-
-				//If elements is created
-				if (elem_id) {
-
-					var end_elem_id = elem_id;
-					Template.AggregateWizard.endClassId.set(elem_id);
-
-					//New element: Name compartment
-					var end_elem = Elements.findOne({_id: end_elem_id});
-					var compart_type = CompartmentTypes.findOne({name: "Name", elementTypeId: end_elem.elementTypeId});
-
-					if (compart_type){
-
-						var condition_compartment = {
-											compartment: {
-
-												projectId: Session.get("activeProject"),
-												versionId: Session.get("versionId"),
-
-												diagramId: Session.get("activeDiagram"),
-												diagramTypeId: compart_type["diagramTypeId"],
-												elementTypeId: compart_type["elementTypeId"],
-
-												compartmentTypeId: compart_type._id,
-												elementId: end_elem_id,
-
-												index: compart_type.index,
-												input: class_name,
-												value: class_name,
-												isObjectRepresentation: false,
-
-												style: compart_type.styles[0]["style"],
-												styleId: compart_type.styles[0]["id"],
-											},
-										};
-
-						Utilities.callMeteorMethod("insertCompartment", condition_compartment);
-
-						//Change type from query to condition
-						var ct = CompartmentTypes.findOne({name: "ClassType"});
-
-						if (ct) {
-							var c = Compartments.findOne({elementId: end_elem_id, compartmentTypeId: ct["_id"]});
-
-							if (c) {
-								var compart_id = c["_id"];
-								Dialog.updateCompartmentValue(ct, "condition", "condition", compart_id);
-							}
-						}
-
-					}
-
-
-					//New line
-					var line_type = ElementTypes.findOne({name: "Link"});
-
-					var line_style = line_type["styles"][0];
-					if (!line_style) {
-						show_error_msg("Internal error: No element style");
-						return;
-					}
-
-	//coordinates for 2 boxes one below other
-					var ix = x0+Math.round(w/2);
-					var a = y0+h;
-					var b = y1;
-
-					if (line_direct == "=>") {
-
-							var new_line = {
-									projectId: Session.get("activeProject"),
-									versionId: Session.get("versionId"),
-
-									diagramId: Session.get("activeDiagram"),
-									diagramTypeId: line_type["diagramTypeId"],
-									elementTypeId: line_type["_id"],
-
-									style: {startShapeStyle: line_style["startShapeStyle"],
-											endShapeStyle: line_style["endShapeStyle"],
-											elementStyle: line_style["elementStyle"],
-											lineType: line_type["lineType"],
-										},
-
-									styleId: line_style["id"],
-									type: "Line",
-									points: [ix, a, ix, b],
-									startElement: start_elem_id,
-									endElement: end_elem_id,
-								};
-
-					} else {
-
-							var new_line = {
-									projectId: Session.get("activeProject"),
-									versionId: Session.get("versionId"),
-
-									diagramId: Session.get("activeDiagram"),
-									diagramTypeId: line_type["diagramTypeId"],
-									elementTypeId: line_type["_id"],
-
-									style: {startShapeStyle: line_style["startShapeStyle"],
-											endShapeStyle: line_style["endShapeStyle"],
-											elementStyle: line_style["elementStyle"],
-											lineType: line_type["lineType"],
-										},
-
-									styleId: line_style["id"],
-									type: "Line",
-									points: [ix, b, ix, a],
-									startElement: end_elem_id,
-									endElement: start_elem_id,
-
-								};
-					}
-
-					Utilities.callMeteorMethod("insertElement", new_line, function(new_line_id) {
-
-						var line_id = new_line_id;
-						var line_elem = Elements.findOne({_id: line_id});
-						var vq_line = new VQ_Element(line_id);
-						vq_line.setLinkType("REQUIRED");
-						if (linkType == "JOIN") vq_line.setNestingType("PLAIN");
-						else if (linkType == "NESTED") vq_line.setNestingType("SUBQUERY");
-						//vq_line.setName(name);
-						var line_compart_type = CompartmentTypes.findOne({name: "Name", elementTypeId: line_elem["elementTypeId"]})
-						if (line_compart_type) {
-
-							var name_compartment = {
-												compartment: {
-
-													projectId: Session.get("activeProject"),
-													versionId: Session.get("versionId"),
-
-													diagramId: Session.get("activeDiagram"),
-													diagramTypeId: line_compart_type["diagramTypeId"],
-													elementTypeId: line_compart_type["elementTypeId"],
-
-													compartmentTypeId: line_compart_type["_id"],
-													elementId: line_id,
-													index: line_compart_type.index,
-													input: name,
-													value: name,
-													isObjectRepresentation: false,
-
-													style: line_compart_type.styles[0]["style"],
-													styleId: line_compart_type.styles[0]["id"],
-												},
-												elementStyleUpdate: undefined,
-											};
-
-							Utilities.callMeteorMethod("insertCompartment", name_compartment);
-
-
-						}
-						var proj = Projects.findOne({_id: Session.get("activeProject")});
-						if (proj) {
-							  console.log(Session.get("activeProject"), proj, proj.autoHideDefaultPropertyName);
-							  if (proj.autoHideDefaultPropertyName=="true") {
-								vq_line.hideDefaultLinkName(true);
-								vq_line.setHideDefaultLinkName("true");
-							  };
-						}
-
-					});
-
-				}
-
-			});*/
-
 			if (document.getElementById("goto-wizard").checked == true ){
 
 				//Fields
@@ -399,51 +138,79 @@ Template.AddLink.events({
 		clearAddLinkInput();
 	},
 
-	"change #type-choice": function() {
+	"click #add-long-link": function() {
+		//Hide Add Link 
+		clearAddLinkInput();
+		$("#add-link-form").modal("hide");
+		//Generate data for Connect Classes
+		var schema = new VQ_Schema();
+		var data = [];
+		var count = 0;
+		_.each(schema.getAllClasses(), function(c){
+			data.push({name: c.name, id: count});
+			count++;
+		});
+		console.log(data);
+		Template.ConnectClasses.elements.set(data);
+		Template.ConnectClasses.addLongLink.set({data: true});
+		$("#connect-classes-form").modal("show");
+		console.log("Connect classes activated");
+	},
+
+//Menu listeners
+	"click #type-choice": function() {
 		var checkedName = $('input[name=type-radio]').filter(':checked').val(); // console.log(checkedName);
         if (checkedName === 'JOIN') {
-            $('#goto-wizard:checked').attr('checked', false);
+            //$('#goto-wizard:checked').attr('checked', false);
             $('#goto-wizard').attr('disabled',"disabled");
         } else {
             $('#goto-wizard').removeAttr("disabled");
+            $('#goto-wizard').attr('checked', true);
         } 
 	},
 
 	"click #link-list-form": function() {
 		$("div[id=errorField]").remove();
-		/*var proj = Projects.findOne({_id: Session.get("activeProject")});
-        if (proj) {
-            if(proj.showCardinalities=="true"){            
-                $('#link-list-form').change(function(){
-                    var checkedName = $('input[name=stack-radio]').filter(':checked').attr("card"); //console.log(checkedName);
-                    if (checkedName.indexOf("[*]") == -1){//max cardinality not [*]
-                        $('input[value=JOIN]').prop('checked', true);
-                        $('input[value=NESTED]').prop('checked', false);
-                        $('input[value=NESTED]').attr('disabled',"disabled");
-                        $('#goto-wizard:checked').attr('checked', false);
-                        $('#goto-wizard').attr('disabled',"disabled");
-                    } else {
-                        $('input[value=NESTED]').removeAttr('disabled');
-                        $('input[value=NESTED]').prop('checked', true);
-                        $('#goto-wizard').removeAttr('disabled');
-                    }                
-                });
-            }
-        }*/
 	},
 
 	"keyup #mySearch": function(){
 		$("div[id=errorField]").remove();
 		var value = $("#mySearch").val().toLowerCase();
-		if (value == "" || value.indexOf(' ') > -1) {//empty or contains space
+		if (value == "" || value == " ") {//empty or space - show all elements
 			Template.AddLink.shortList.set(Template.AddLink.fullList.curValue);
+		} else if (value.indexOf('.') > -1) {
+			console.log("TODO property path");
 		} else {
+			if (value.indexOf(' ') > -1 || value.indexOf(',') > -1) {
+				value = value.replace(/,/g, ' ').replace(/ {2,}/g, ' ');
+				value = value.split(" ");			
+			} else {
+				value = [value];
+			}
+			value = value.filter(function(e) { return e !== "" });
+
 			var ascList = Template.AddLink.fullList.curValue;
 			ascList = ascList.filter(function(e){ //{name: "++", class: " ", type: "=>", card: "", clr: ""}				
-				return e.name.toLowerCase().indexOf(value) > -1 || e.class.toLowerCase().indexOf(value) > -1;
+				var hasValues = true;
+				_.each(value, function(v){ //check if any of searched values is missing
+					if (e.name.toLowerCase().indexOf(v) == -1 && e.class.toLowerCase().indexOf(v) == -1) {
+						hasValues = false;
+					}
+				});
+				return hasValues;
 			})
 			Template.AddLink.shortList.set(ascList);
 		}
+//Original
+		// if (value == "" || value.indexOf(' ') > -1) {//empty or contains space
+		// 	Template.AddLink.shortList.set(Template.AddLink.fullList.curValue);
+		// } else {
+		// 	var ascList = Template.AddLink.fullList.curValue;
+		// 	ascList = ascList.filter(function(e){ //{name: "++", class: " ", type: "=>", card: "", clr: ""}				
+		// 		return e.name.toLowerCase().indexOf(value) > -1 || e.class.toLowerCase().indexOf(value) > -1;
+		// 	})
+		// 	Template.AddLink.shortList.set(ascList);
+		// }
 	},
 
 });
@@ -603,18 +370,4 @@ function getAllAssociations(){
 			return asc;
 		}
 }
-
-// function getDetailedAttributes() {
-// 	var schema = new VQ_Schema();
-// 	var detailedList = [];
-
-// 	_.each(schema.Associations, function(e) {
-// 		_.each(e.schemaRole, function(r){
-// 			if (e.localName && e.localName != " ")
-// 				detailedList.push({name: e.localName, min: r.minCardinality, max: r.maxCardinality, 
-// 									from: r.sourceClass.localName, to: r.targetClass.localName});
-// 		})				
-// 	})
-// 	return detailedList;
-// }
 
