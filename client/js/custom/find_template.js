6dd94665db41f5782ba5835561a90c1f45d4285d
elinakalnina <elina.kalnina@gmail.com>
2020-03-23 16:26:52 +0200

    Clean up

--- client/js/custom/find_template.js
+++ client/js/custom/find_template.js
@@ -6,24 +6,20 @@ Template.findResult.helpers({
 	},
 
 	json2: function() {
-		console.log(Session.get("json"))
 		return Session.get("json");
 	},
 
 	Potential: function() {
-		console.log(Session.get("PotentialResults"))
 		return Session.get("PotentialResults");
 	},
 
 	ViolatedConstiants: function() {
-		console.log(Session.get("ViolatedConstiants"))
 		return Session.get("ViolatedConstiants");
 	},
 });
 
 Template.findResult.events({
 	'click .delete': function(e) {            
-				console.log("Click delete", this);
 				var list = {compartmentId: this.compartmentId,
 					  diagramId: Session.get("activeDiagram"),
 					   projectId: Session.get("activeProject"), 
@@ -33,8 +29,5 @@ Template.findResult.events({
 							  Session.set("PotentialResults", resp.potentialDiagIds);
 							  Session.set("ViolatedConstiants", resp.violatedConstraints);
 				}); 
-
-			//	Compartments.remove(this.compartmentId);
-		
-				}
+			}
 	})
