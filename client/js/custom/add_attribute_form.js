98b579260155b529a158dfc1a87d43c9932d2eee
Jūlija Ovčiņņikova <julija.ovcinnikova@lumii.lv>
2021-01-15 09:21:07 +0200

    Add Attribute form

--- client/js/custom/add_attribute_form.js
+++ client/js/custom/add_attribute_form.js
@@ -2,6 +2,12 @@ Template.AddAttribute.attrList = new ReactiveVar([{name: "No_attribute"}]);
 Template.AddAttribute.linkList = new ReactiveVar([{name: "No_attribute"}]);
 Template.AddAttribute.existingAttributes = new ReactiveVar([{name: "No_attribute"}]);
 
+Template.AddNewAttribute.alias = new ReactiveVar("");
+Template.AddNewAttribute.expression = new ReactiveVar("");
+Template.AddNewAttribute.requireValues = new ReactiveVar("false");
+Template.AddNewAttribute.helper = new ReactiveVar("false");
+Template.AddNewAttribute.attributeid = new ReactiveVar("");
+
 Interpreter.customMethods({
 	AddAttribute: function () {
 		
@@ -45,41 +51,39 @@ Template.AddAttribute.events({
 		if (Elements.findOne({_id: selected_elem_id})){ //Because in case of deleted element ID is still "activeElement"
 		//Read user's choise
 		  var vq_obj = new VQ_Element(selected_elem_id);
-		  
-		  var field_list = vq_obj.getFields();
+		  console.log("v1", vq_obj, selected_elem_id )
+		  // var field_list = vq_obj.getFields();
 		  // var x = document.getElementsByName("add-attribute");
-		  var x = document.getElementsByClassName("information");
-		  for(var existingDiagramField in field_list){
-			var fieldFound = false;
-			for(var existingField in x){
-				if(typeof x[existingField] === "object"){
-					if(x[existingField].getAttribute("name") == field_list[existingDiagramField]["_id"]){
-						if(x[existingField].parentNode.style.display != "none"){
-							fieldFound = true;
-							
-							console.log("aaaaa", x[existingField]);
-							
-							break;
-						} else x[existingField].parentNode.style.display = "table-row";
-					}
-				}
+		  // var x = document.getElementsByClassName("information");
+		  // for(var existingDiagramField in field_list){
+			// var fieldFound = false;
+			// for(var existingField in x){
+				// if(typeof x[existingField] === "object"){
+					// if(x[existingField].getAttribute("name") == field_list[existingDiagramField]["_id"]){
+						// if(x[existingField].parentNode.style.display != "none"){
+							// fieldFound = true;
+							// break;
+						// } else x[existingField].parentNode.style.display = "table-row";
+					// }
+				// }
 				
-			}
-			if(fieldFound == false){
-				var list = {compartmentId: field_list[existingDiagramField]["_id"],
-					projectId: Session.get("activeProject"),
-					versionId: Session.get("versionId"),
-				};
+			// }
+			// if(fieldFound == false){
+				// var list = {compartmentId: field_list[existingDiagramField]["_id"],
+					// projectId: Session.get("activeProject"),
+					// versionId: Session.get("versionId"),
+				// };
 
-				Utilities.callMeteorMethod("removeCompartment", list);
-			}
-		  }
+				// Utilities.callMeteorMethod("removeCompartment", list);
+			// }
+		  // }
 		  
-		  var checkboxes = $('input[name=stack-checkbox]:checked').closest(".attribute");
-		  checkboxes.each(function () {
-		    var required = $(this).children('label[name="add-attribute"]').children('button[name="required-attribute"]').prop("innerHTML")
-
-			if(required == "+") required = true;
+		  
+		  var buttonn = $('button[name=required-attribute-to-add]').closest(".attribute");
+		  buttonn.each(function () {
+		    
+			var required = $(this).children('label[name="add-attribute"]').children('button[name="required-attribute-to-add"]').attr("class");
+			if(required.startsWith("fa fa-plus")) required = true;
 			else required = false;
 			
 			var name = $(this).attr("name");
@@ -101,31 +105,79 @@ Template.AddAttribute.events({
 	},
 	
 	"click #required-attribute": function(e) {
-		
-		if(e.target.innerHTML == "+") {
-			e.target.innerHTML = '\&nbsp;';
-		}
-		else {
-			e.target.innerHTML = "+";
-			$(e.target.parentElement).children('input[name="stack-checkbox"]').prop("checked", true);
-		}
+		console.log($(e.target).attr('name'));
 
+		if(e.target.className == "button button-required") {
+			e.target.className = "fa fa-check button button-required";
+			$(e.target).attr('name', "required-attribute-to-add");
+		}
+		else if(e.target.className == "fa fa-check button button-required") {
+			e.target.className = "fa fa-plus button button-required";
+			$(e.target).attr('name', "required-attribute-to-add");
+		}
+		else if(e.target.className == "fa fa-plus button button-required"){
+			e.target.className = "button button-required";
+			$(e.target).attr('name', "required-attribute");
+		}
+		
 		return;
 
 	},
 	
 	"click #required-existing-attribute": function(e) {
-		var labelText = e.target.parentNode.childNodes[3].textContent;
-		if(e.target.innerHTML == "+") {
-			e.target.innerHTML = '\&nbsp;';
-			e.target.parentNode.childNodes[3].textContent=labelText.substring(3);
-		}
-		else {
-			e.target.innerHTML = "+";
-			e.target.parentNode.childNodes[3].textContent="{+} " + labelText;
+	
+		var labelText = $(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent;
+		if(labelText.startsWith("{+}")) {
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = labelText.substring(4);
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("requireValues", "");
+		}else if(labelText.startsWith("{h} {+}")) {
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = "{h} "+labelText.substring(7);
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("requireValues", "");
+		}else if(labelText.startsWith("{h}")){
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = "{h} {+} "+labelText.substring(3);
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("requireValues", "checked");
+		}else {
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = "{+} "+labelText;
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("requireValues", "checked");
 		}
 		return;
+	},
+	
+	"click #attribute-helper-button": function(e) {
+		var labelText = $(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent;
+		if(labelText.startsWith("{+}")){
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = "{h} "+labelText;
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("helper", "checked");
+		}else if(labelText.startsWith("{h} {+}")) {
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = labelText.substring(4);
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("helper", "");
+		}else if(labelText.startsWith("{h}")){
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = labelText.substring(4);
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("helper", "");
+		}else {
+			$(e.target).closest(".attribute")[0].childNodes[1].childNodes[1].childNodes[1].textContent = "{h} "+labelText;
+			$(e.target).closest(".attribute")[0].childNodes[1].setAttribute("helper", "checked");
+		}
+		
+		// var act_elem = Session.get("activeElement");
+			// var act_el = Elements.findOne({_id: act_elem}); //Check if element ID is valid
+			// if(typeof act_el !== 'undefined'){
+				// var compart_type = CompartmentTypes.findOne({name: "Attributes", elementTypeId: act_el["elementTypeId"]});
+				// var value = Dialog.buildCompartmentValue(compart_type, fullText, fullText);
+				// var compart = Compartments.findOne({compartmentTypeId: compart_type["_id"], elementId: act_elem});
 
+				// compart.subCompartments["Attributes"]["Attributes"]["Expression"]["value"] = expression;
+				// compart.subCompartments["Attributes"]["Attributes"]["Expression"]["input"] = expression;
+				// if(alias!="" || alias!=null)compart.subCompartments["Attributes"]["Attributes"]["Field Name"]["value"] = alias+"<-";
+				// compart.subCompartments["Attributes"]["Attributes"]["Field Name"]["input"] = alias;
+				// if(helper==true)compart.subCompartments["Attributes"]["Attributes"]["IsInternal"]["value"] = "{h} ";
+				// compart.subCompartments["Attributes"]["Attributes"]["IsInternal"]["input"] = helper.toString() ;
+				// if(requireValues==true)compart.subCompartments["Attributes"]["Attributes"]["Require Values"]["value"] = "{+} ";
+				// compart.subCompartments["Attributes"]["Attributes"]["Require Values"]["input"] = requireValues.toString() ;
+				
+				// Dialog.updateCompartmentValue(compart_type, fullText, value, $(document.getElementById("add-new-attribute-alias")).closest(".multi-field")[0].getAttribute("attributeid"), null, null, compart.subCompartments);
+			// }
+		return;
 	},
 	
 	"click #hide-existing-attribute": function(e) {
@@ -165,8 +217,18 @@ Template.AddAttribute.events({
 	
 	"click #attribute-delete-button": function(e) {
 		
+		// console.log($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("name"));
+		
 		$(e.target).closest(".attribute")[0].style.display = "none";
-		// $(e.target).closest(".attribute").remove();
+		
+		
+		var list = {compartmentId: $(e.target).closest(".attribute")[0].childNodes[1].getAttribute("name"),
+					projectId: Session.get("activeProject"),
+					versionId: Session.get("versionId"),
+				};
+
+				Utilities.callMeteorMethod("removeCompartment", list);
+		// $(e.target).closest(".attribute")[0].remove();
 		
 		return;
 
@@ -192,6 +254,126 @@ Template.AddAttribute.events({
 			return typeof obj['name'] === 'undefined' || obj['name'].toLowerCase().indexOf(value)!== -1;
 		});
 		Template.AddAttribute.linkList.set(link_list);
+	},
+	
+	'click #attribute-new-button': function(e) {
+		Template.AddNewAttribute.alias.set("");
+		Template.AddNewAttribute.expression.set("");
+		Template.AddNewAttribute.requireValues.set("");
+		Template.AddNewAttribute.helper.set("");
+		Template.AddNewAttribute.attributeid.set("newAttribute");
+		$("#add-new-attribute-form").modal("show");
+		return;
+	},
+	
+	'click #attribute-extra-button': function(e) {
+		
+		Template.AddNewAttribute.alias.set($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("alias"));
+		Template.AddNewAttribute.expression.set($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("expression"));
+		Template.AddNewAttribute.requireValues.set($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("requireValues"));
+		Template.AddNewAttribute.helper.set($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("helper"));		
+		Template.AddNewAttribute.attributeid.set($(e.target).closest(".attribute")[0].childNodes[1].getAttribute("name"));		
+		$("#add-new-attribute-form").modal("show");
+		return;
+	},
+});
+
+Template.AddNewAttribute.helpers({
+
+	alias: function() {
+		return Template.AddNewAttribute.alias.get();
+	},
+	
+	expression: function() {
+		return Template.AddNewAttribute.expression.get();
+	},
+	
+	requireValues: function() {
+		return Template.AddNewAttribute.requireValues.get();
+	},
+	
+	helper: function() {
+		return Template.AddNewAttribute.helper.get();
+	},
+
+	attributeid: function() {
+		return Template.AddNewAttribute.attributeid.get();
+	},
+	
+});
+
+Template.AddNewAttribute.events({
+
+	"click #ok-add-new-attribute": function(e, t) {
+		console.log("eee", e, t);
+		var alias = document.getElementById("add-new-attribute-alias").value;
+		var expression = document.getElementById("add-new-attribute-expression").value;
+		var requireValues = document.getElementById("add-new-attribute-requireValues").checked ;
+		var helper = document.getElementById("add-new-attribute-helper").checked ;
+		var fullText = "";
+		if(helper == true) fullText = "{h} ";
+		if(requireValues == true) fullText = fullText + "{+} ";
+		if(alias != null && alias != "") fullText = fullText + alias + "<-";
+		fullText = fullText + expression;
+		
+		console.log(alias, $(document.getElementById("add-new-attribute-alias")).closest(".multi-field")[0].getAttribute("attributeid"))
+		if($(document.getElementById("add-new-attribute-alias")).closest(".multi-field")[0].getAttribute("attributeid") == "newAttribute"){
+			
+			console.log(document.getElementById("resp-table-body"));
+			str = '<div class="resp-table-row attribute">'+
+									'<div style="display: table-cell;" class="table-body-cell information" name="newAttribute" alias="'+alias+'" expression="'+expression+'" requireValues="'+requireValues+'" helper="'+helper+'">'+
+										'<label class="label-checkbox" name="add-attribute">'+
+											'<span>'+fullText+'</span>'+
+										'</label>'+
+									'</div>'+
+									'<div style="display: table-cell;" class="table-body-cell">'+
+										'<a class="btn btn-xs btn-warning edit-multi-field"><i id="attribute-extra-button" class="fa fa-pencil"></i></a>'+
+										'<a class="btn btn-xs btn-default"><i id="attribute-helper-button" class="fa">h</i></a>'+
+										'<a class="btn btn-xs btn-default"><i id="required-existing-attribute" class="fa fa-plus"></i></a>'+
+										'<a class="btn btn-xs btn-info up-multi-field"><i id="attribute-muve-button" class="fa fa-arrow-up"></i></a>'+
+										'<a class="btn btn-xs btn-danger remove-multi-field"><i id="attribute-delete-button" class="fa fa-trash-o"></i></a>'+
+									'</div>'+
+								'</div>'
+			document.getElementById("resp-table-body").insertAdjacentHTML( 'beforeend', str );
+			var selected_elem_id = Session.get("activeElement");
+			if (Elements.findOne({_id: selected_elem_id})){ //Because in case of deleted element ID is still "activeElement"
+				var vq_obj = new VQ_Element(selected_elem_id);
+				vq_obj.addField(expression,alias,requireValues,false,helper);
+			};
+		} else {
+			var attribute = document.getElementsByName($(document.getElementById("add-new-attribute-alias")).closest(".multi-field")[0].getAttribute("attributeid"))[0];
+			attribute.setAttribute("alias", alias);
+			attribute.setAttribute("expression", expression);
+			attribute.setAttribute("requireValues", requireValues);
+			attribute.setAttribute("helper", helper);
+			attribute.childNodes[1].childNodes[1].textContent = fullText;
+
+			var act_elem = Session.get("activeElement");
+			var act_el = Elements.findOne({_id: act_elem}); //Check if element ID is valid
+			if(typeof act_el !== 'undefined'){
+				var compart_type = CompartmentTypes.findOne({name: "Attributes", elementTypeId: act_el["elementTypeId"]});
+				var value = Dialog.buildCompartmentValue(compart_type, fullText, fullText);
+				var compart = Compartments.findOne({compartmentTypeId: compart_type["_id"], elementId: act_elem});
+
+				compart.subCompartments["Attributes"]["Attributes"]["Expression"]["value"] = expression;
+				compart.subCompartments["Attributes"]["Attributes"]["Expression"]["input"] = expression;
+				if(alias!="" || alias!=null)compart.subCompartments["Attributes"]["Attributes"]["Field Name"]["value"] = alias+"<-";
+				compart.subCompartments["Attributes"]["Attributes"]["Field Name"]["input"] = alias;
+				if(helper==true)compart.subCompartments["Attributes"]["Attributes"]["IsInternal"]["value"] = "{h} ";
+				compart.subCompartments["Attributes"]["Attributes"]["IsInternal"]["input"] = helper.toString() ;
+				if(requireValues==true)compart.subCompartments["Attributes"]["Attributes"]["Require Values"]["value"] = "{+} ";
+				compart.subCompartments["Attributes"]["Attributes"]["Require Values"]["input"] = requireValues.toString() ;
+				
+				Dialog.updateCompartmentValue(compart_type, fullText, value, $(document.getElementById("add-new-attribute-alias")).closest(".multi-field")[0].getAttribute("attributeid"), null, null, compart.subCompartments);
+			}
+		}
+		return;
+	},
+	
+	"click #cancel-add-new-attribute": function(e) {
+		
+		return;
+
 	},
 });
 
@@ -306,22 +488,20 @@ function getExistingAttributes(){
 	if (Elements.findOne({_id: selected_elem_id})){ //Because in case of deleted element ID is still "activeElement"
 		var vq_obj = new VQ_Element(selected_elem_id);
 		var field_list = vq_obj.getFields().map(function(f) {
-			
-			var r = " ";
+			var al = f.alias;
+			if(al==null) al="";
+			var r = "";
 			if(f.requireValues == true) {
-				r = "+";
+				r = "checked";
 			}
 			var fulltext = f.fulltext;
-			var hide = "";
-			var hideBox = "";
+			var hide = "false";
 			// if(typeof f.alias !== 'undefined' && f.alias != "") fulltext = f.alias + "<-" + fulltext;
 			if(f.isInternal == true) {
-				// fulltext = "{hide} " + fulltext;
-				hide = "{hide}";
-				hideBox = "checked";
+				hide = "checked";
 			}
 			
-			return {name:f.exp, required: r, fulltext:fulltext, alias:f.alias, hideCheckBox:hideBox, id:f._id}});
+			return {name:f.exp, requireValues: r, fulltext:fulltext, al:al, hel:hide, id:f._id}});
 		return field_list;
 	}
 	return [];
