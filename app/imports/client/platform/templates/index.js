0c0f400068641e9578fbb7ac8ffc25675b87ff30
mikus <mikusg@gmail.com>
2023-12-17 02:53:05 +0200

    pielabots templates importa saraksts

--- app/imports/client/platform/templates/index.js
+++ app/imports/client/platform/templates/index.js
@@ -9,22 +9,23 @@ import './account/reset_password_page.html'
 import './archive/archive.js'
 
 // configurator
-import './configurator/compartments/extensions.js'
 import './configurator/compartments/main.js'
+import './configurator/compartments/extensions.js'
 import './configurator/compartments/reorder.js'
 import './configurator/compartments/styles.js'
 import './configurator/compartments/widgets.js'
 
+import './configurator/diagrams/imported_translets.html'
+import './configurator/diagrams/palette.html'
 import './configurator/diagrams/context_menus.js'
 import './configurator/diagrams/extension_points.js'
-import './configurator/diagrams/imported_translets.html'
 import './configurator/diagrams/keystrokes.js'
-import './configurator/diagrams/main.js'
-import './configurator/diagrams/palette.html'
 import './configurator/diagrams/settings.js'
 import './configurator/diagrams/styles.js'
 import './configurator/diagrams/toolbar.js'
+import './configurator/diagrams/main.js'
 
+import './configurator/elements/styles.js'
 import './configurator/elements/box_styles.js'
 import './configurator/elements/context_menus.js'
 import './configurator/elements/dialog_tabs.js'
@@ -32,14 +33,13 @@ import './configurator/elements/extension_points.js'
 import './configurator/elements/keystrokes.js'
 import './configurator/elements/line_styles.js'
 import './configurator/elements/main.js'
-import './configurator/elements/styles.js'
 
+import './configurator/tools/toolbar.html'
 import './configurator/tools/diagrams.js'
 import './configurator/tools/documents.js'
-import './configurator/tools/main.js'
 import './configurator/tools/ribbon.js'
 import './configurator/tools/timeline.js'
-import './configurator/tools/toolbar.html'
+import './configurator/tools/main.js'
 
 import './configurator/configurator.js'
 import './configurator/config_utils.js'
@@ -48,14 +48,14 @@ import './configurator/element.js'
 
 
 // diagrams
+import './diagrams/dialog/value_from_subcompartments.html'
 import './diagrams/dialog/generate_dialog.js'
 import './diagrams/dialog/multiField.html'
 import './diagrams/dialog/subCompartments.js'
 import './diagrams/dialog/swimlane.js'
-import './diagrams/dialog/value_from_subcompartments.html'
 
-import './diagrams/diagram.js'
 import './diagrams/diagram_log.html'
+import './diagrams/diagram.js'
 import './diagrams/diagrams.js'
 import './diagrams/public_diagram.js'
 import './diagrams/toolbar.js'
@@ -69,12 +69,6 @@ import './home/home.html'
 
 // index
 import './index/index.js'
-
-// main
-import './main/confirm.js'
-import './main/noProject.js'
-import './main/notFound.html'
-import './main/search.js'
 
 // notifications
 import './notifications/notifications.js'
@@ -98,3 +92,10 @@ import './users/users.js'
 
 // .
 import './publicLayout.html'
+
+// main
+import './main/confirm.js'
+import './main/noProject.js'
+import './main/notFound.html'
+import './main/search.js'
+
