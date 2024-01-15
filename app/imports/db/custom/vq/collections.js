939cd7b3e3dcd17847e6fe6cd8f9a7b84695e48b
LeldeLace <lelde@latnet.lv>
2024-01-15 13:31:11 +0200

    code cleaning - unused collections

--- app/imports/db/custom/vq/collections.js
+++ app/imports/db/custom/vq/collections.js
@@ -2,18 +2,12 @@ import { Mongo } from 'meteor/mongo';
  
 // Custom collections
 const TriplesMaps = new Mongo.Collection("TriplesMaps");
-const Associations = new Mongo.Collection("Associations");
-const Attributes = new Mongo.Collection("Attributes");
-const Classes = new Mongo.Collection("Classes");
 const Schema = new Mongo.Collection("Schema");
 const Services = new Mongo.Collection("Services");
 
 const VQ_sparql_logs = new Mongo.Collection("VQ_Exec_SPARQL_Logs");
 
 export {
-	Associations,
-	Attributes,
-	Classes,
 	Schema,
 	TriplesMaps,
 	Services,
