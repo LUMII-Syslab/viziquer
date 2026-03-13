4f188d35198b134a5c4c63fa6eeed29f137f7197
mikus <mikusg@gmail.com>
2026-03-13 14:33:59 +0200

    platform branding

--- app/client/js/_tempInitGlobals.js
+++ app/client/js/_tempInitGlobals.js
@@ -3,5 +3,5 @@ Session.set('_toolName', 'ViziQuer');
 Session.set('_toolSiteName', 'ViziQuer Site');
 Session.set('_toolSiteURL', 'https://viziquer.lumii.lv');
 
-Session.set('_platformName', 'ViziQuer and OWLGrEd Tool Building Platform');
+Session.set('_platformName', 'Visual Semantic Technology Platform');
 Session.set('_platformVersion', '1.2.3');
