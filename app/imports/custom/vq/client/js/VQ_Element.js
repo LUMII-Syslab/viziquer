988334fe4671ffaf8434c7cea3575f8c5fea4599
Dārta Gobleja <dartagobleja@gmail.com>
2026-03-13 14:20:32 +0200

    Bug fix: if "Attributes" compartment does not exist, return empty field list

--- app/imports/custom/vq/client/js/VQ_Element.js
+++ app/imports/custom/vq/client/js/VQ_Element.js
@@ -2398,7 +2398,7 @@ class VQ_Element_Async{
     {title:"graph",name:"Graph"},
     {title:"graphInstruction",name:"Graph instruction"},
     {title:"attributeConditionSelection",name:"AttributeConditionSelection"},
-	{title:"attributeCondition",name:"Attribute Condition", transformer:function(v) {return v=="true"}},
+	  {title:"attributeCondition",name:"Attribute Condition", transformer:function(v) {return v=="true"}},
     {title:"nodeLevelCondition",name:"Node-level Condition", transformer:function(v) {return v=="true"}},
     {title:"requireValues",name:"Require Values",transformer:function(v) {return v=="true"}},
     {title:"addLabel",name:"Add Label",transformer:function(v) {return v=="true"}},
@@ -2407,22 +2407,24 @@ class VQ_Element_Async{
 		{title:"groupValues",name:"GroupValues",transformer:function(v) {return v=="true"}},
 	  {title:"isInternal",name:"IsInternal",transformer:function(v) {return v=="true"}}]);
 
-	const compart_type = await CompartmentTypes.findOneAsync({name: "Attributes", elementTypeId: this.obj.elementTypeId})
-	const compart_type_id = compart_type["_id"];
-	var compartments = await Compartments.find({compartmentTypeId: compart_type_id, elementId: this.obj._id, }, {sort: {index: 1}}).fetchAsync();
+    var compratmentList = [];
 
-	var compratmentList = [];
+    const compart_type = await CompartmentTypes.findOneAsync({ name: "Attributes", elementTypeId: this.obj.elementTypeId })
+    if (compart_type) {
+      const compart_type_id = compart_type["_id"];
+      var compartments = await Compartments.find({ compartmentTypeId: compart_type_id, elementId: this.obj._id, }, { sort: { index: 1 } }).fetchAsync();
 
-	for(var compartment of compartments){
-		for(var field of field_list){
-			if(field["_id"] == compartment["_id"]) {
-				compratmentList.push(field);
-				break;
-			}
-		}
-	}
+      for (var compartment of compartments) {
+        for (var field of field_list) {
+          if (field["_id"] == compartment["_id"]) {
+            compratmentList.push(field);
+            break;
+          }
+        }
+      }
+    }
 
-	return compratmentList;
+	  return compratmentList;
   }
 
   async addField(exp,alias,requireValues,groupValues,isInternal,addLabel,addAltLabel,addDescription,graph,graphInstruction, condition, isAttributeCondition, isNodeLevelCondition) {
