baba52f57b9b0e5854cfffe922eab0a6c188dafe
Sakars <o.putaans@gmail.com>
2026-06-02 22:02:57 +0000

    Soft reset to cancel whitespace overwrite

--- app/imports/custom/vq/client/templates/sparql_form.js
+++ app/imports/custom/vq/client/templates/sparql_form.js
@@ -448,7 +448,7 @@ class VqDSSRequestProvider {
 	 */
 	async getOntologyList() {
 		/** @type { {id: number, display_name: string, db_schema_name: string, schema_name: string, sparql_url: string}[] } */
-		const ontologies = await dataShapes.getOntologies();
+		const ontologies = dataShapes.getOntologiesSync();
 		return ontologies.map(o => ({
 			dbSchemaName: o.db_schema_name,
 			name: o.display_name,
@@ -492,8 +492,10 @@ function dssClientCompleter() {
 		async: true,
 		name: "customPropertyCompleter",
 		isValidCompletionPosition: (yasqe) => {
-			const isValid = defaultPropertyCompleter.isValidCompletionPosition(yasqe);
-			return isValid ?? false;
+			const token = yasqe.getCompleteToken();
+			if (token.string[0] === "?" || token.string[0] === "$") return false; // we are typing a var
+			if (token.state.possibleCurrent.indexOf("a") >= 0) return true; // predicate pos
+			return false;
 		},
 		preProcessToken(yasqe, token) {
 			return preprocessIriForCompletion(yasqe, token);
